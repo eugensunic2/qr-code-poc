9ad2a6de1c40516bb61b7f5132d10a099460dd62
Alan Kinkela Ramirez <akinkelar@outlook.com>
2020-01-08 14:52:13 +0100

    css for language buttons

--- fe-cms/src/App.js
+++ fe-cms/src/App.js
@@ -1,4 +1,7 @@
-import React, { useReducer, useEffect } from 'react';
+import React, {
+	useReducer,
+	useEffect
+} from 'react';
 import Footer from './components/utils/Footer';
 import PrivateRoute from './components/utils/PrivateRoute';
 import Register from './components/main/Register';
@@ -11,7 +14,11 @@ import CreateContent from './components/main/CreateContent';
 import OverviewContent from './components/main/OverviewContent';
 import About from './components/main/About';
 import AdminPage from './components/main/Admin';
-import { Router, Route, Switch } from 'react-router-dom';
+import {
+	Router,
+	Route,
+	Switch
+} from 'react-router-dom';
 import { history, isLoggedIn } from './helpers';
 import ForgotPassword from './components/main/ForgotPassword';
 import ChangePassword from './components/main/ChangePassword';
@@ -20,79 +27,99 @@ import { useTranslation } from 'react-i18next';
 import { IMAGE_HOST_PREFIX } from './config';
 
 // global error reducer
-const reducer = (state = { message: '' }, action) => {
-  switch (action.type) {
-    case 'global':
-      return {
-        ...state,
-        message: action.payload
-      };
-    default:
-      return state;
-  }
+const reducer = (
+	state = { message: '' },
+	action
+) => {
+	switch (action.type) {
+		case 'global':
+			return {
+				...state,
+				message: action.payload
+			};
+		default:
+			return state;
+	}
 };
-export const GlobalErrorContext = React.createContext({});
+export const GlobalErrorContext = React.createContext(
+	{}
+);
 
-const isVisitorPage = path => path.indexOf(ROUTES.VISITOR_PAGE) > -1;
+const isVisitorPage = (path) =>
+	path.indexOf(ROUTES.VISITOR_PAGE) > -1;
 const shouldDisplayHeader = () =>
-  (isVisitorPage(history.location.pathname) && isLoggedIn()) ||
-  !isVisitorPage(history.location.pathname);
+	(isVisitorPage(history.location.pathname) &&
+		isLoggedIn()) ||
+	!isVisitorPage(history.location.pathname);
 
 function App() {
-  const { i18n } = useTranslation();
+	const [error, dispatch] = useReducer(reducer, {
+		message: ''
+	});
 
-  const changeLanguage = lng => {
-    localStorage.setItem('app-language', lng);
-    i18n.changeLanguage(lng);
-  };
+	return (
+		<GlobalErrorContext.Provider
+			value={{ dispatchError: dispatch }}>
+			<Router history={history}>
+				{shouldDisplayHeader() && (
+					<div>
+						<Header />
+					</div>
+				)}
+				<ErrorContainer message={error.message} />
 
-  const [error, dispatch] = useReducer(reducer, { message: '' });
-
-  useEffect(() => {
-    changeLanguage(localStorage.getItem('app-language') || 'cro');
-  }, []);
-
-  return (
-    <GlobalErrorContext.Provider value={{ dispatchError: dispatch }}>
-      <Router history={history}>
-        {shouldDisplayHeader() && (
-          <div>
-            <div className="float-right cursor-pointer">
-              <img
-                src={IMAGE_HOST_PREFIX + 'language/uk.png'}
-                alt="english"
-                onClick={() => changeLanguage('en')}
-              />
-              <img
-                src={IMAGE_HOST_PREFIX + 'language/cro.png'}
-                alt="croatian"
-                onClick={() => changeLanguage('cro')}
-              />
-            </div>
-            <Header />
-          </div>
-        )}
-        <ErrorContainer message={error.message} />
-
-        <div className="container">
-          <Switch>
-            <PrivateRoute path={ROUTES.ADMIN} component={AdminPage} />
-            <Route path={ROUTES.VISITOR_PAGE + '/:qrCodeId'} component={VisitorPage} />
-            <PrivateRoute path={ROUTES.CHANGE_PASSWORD} component={ChangePassword} />
-            <Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
-            <Route exact path={ROUTES.HOME} component={Login} />
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <PrivateRoute path={ROUTES.REGISTER} component={Register} />
-            <PrivateRoute path={ROUTES.CREATE} component={CreateContent} />
-            <PrivateRoute path={ROUTES.OVERVIEW} component={OverviewContent} />
-            <Route path={ROUTES.ABOUT} component={About} />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </Router>
-      <Footer />
-    </GlobalErrorContext.Provider>
-  );
+				<div className='container'>
+					<Switch>
+						<PrivateRoute
+							path={ROUTES.ADMIN}
+							component={AdminPage}
+						/>
+						<Route
+							path={
+								ROUTES.VISITOR_PAGE + '/:qrCodeId'
+							}
+							component={VisitorPage}
+						/>
+						<PrivateRoute
+							path={ROUTES.CHANGE_PASSWORD}
+							component={ChangePassword}
+						/>
+						<Route
+							path={ROUTES.FORGOT_PASSWORD}
+							component={ForgotPassword}
+						/>
+						<Route
+							exact
+							path={ROUTES.HOME}
+							component={Login}
+						/>
+						<Route
+							path={ROUTES.LOGIN}
+							component={Login}
+						/>
+						<PrivateRoute
+							path={ROUTES.REGISTER}
+							component={Register}
+						/>
+						<PrivateRoute
+							path={ROUTES.CREATE}
+							component={CreateContent}
+						/>
+						<PrivateRoute
+							path={ROUTES.OVERVIEW}
+							component={OverviewContent}
+						/>
+						<Route
+							path={ROUTES.ABOUT}
+							component={About}
+						/>
+						<Route component={NoMatch} />
+					</Switch>
+				</div>
+			</Router>
+			<Footer />
+		</GlobalErrorContext.Provider>
+	);
 }
 
 export default App;
--- fe-cms/src/components/utils/Navbar.js
+++ fe-cms/src/components/utils/Navbar.js
@@ -1,47 +1,111 @@
-import React, { useState } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React, {
+	useReducer,
+	useEffect,
+	useState
+} from 'react';
+import {
+	Link,
+	withRouter
+} from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { ROUTES } from '../../navigation';
 import { userAccessEndpoint } from '../../config';
 import { useTranslation } from 'react-i18next';
-
+import { IMAGE_HOST_PREFIX } from '../../config';
 function Navbar(route) {
-  const { t } = useTranslation();
-  
-  const [toggleHeader, setToggleHeader] = useState(false);
+	const { t } = useTranslation();
 
-  const logout = () => {
-    fetch(userAccessEndpoint.LOGOUT, {
-      method: 'POST'
-    }).then(_ => route.history.push(ROUTES.HOME));
-  };
+	const [
+		toggleHeader,
+		setToggleHeader
+	] = useState(false);
 
-  return (
-    <nav className={`hero-nav ${!toggleHeader && 'hidden-header'}`}>
-      <Button className="hamburger" onClick={_ => setToggleHeader(!toggleHeader)}>
-        <i className="fa fa-bars"></i>
-      </Button>
-      <ul className="hero-nav-ul" onClick={_ => setToggleHeader(!toggleHeader)}>
-        <li>
-          <Link to={ROUTES.OVERVIEW}>{t('Overview')}</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.CREATE}>{t('New content')}</Link>
-        </li>
-        <li className="dropdownList">
-          <Link to={ROUTES.ADMIN}>{t('Admin')}</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.ABOUT}>{t('About')}</Link>
-        </li>
-        <li className="logoutLi">
-          <button className="logoutButton" onClick={logout}>
-            {t('Logout')}
-          </button>
-        </li>
-      </ul>
-    </nav>
-  );
+	const logout = () => {
+		fetch(userAccessEndpoint.LOGOUT, {
+			method: 'POST'
+		}).then((_) =>
+			route.history.push(ROUTES.HOME)
+		);
+	};
+
+	const { i18n } = useTranslation();
+
+	const changeLanguage = (lng) => {
+		localStorage.setItem('app-language', lng);
+		i18n.changeLanguage(lng);
+	};
+
+	useEffect(() => {
+		changeLanguage(
+			localStorage.getItem('app-language') ||
+				'cro'
+		);
+	}, []);
+
+	return (
+		<nav
+			className={`hero-nav ${!toggleHeader &&
+				'hidden-header'}`}>
+			<Button
+				className='hamburger'
+				onClick={(_) =>
+					setToggleHeader(!toggleHeader)
+				}>
+				<i className='fa fa-bars'></i>
+			</Button>
+			<ul
+				className='hero-nav-ul'
+				onClick={(_) =>
+					setToggleHeader(!toggleHeader)
+				}>
+				<li>
+					<Link to={ROUTES.OVERVIEW}>
+						{t('Overview')}
+					</Link>
+				</li>
+				<li>
+					<Link to={ROUTES.CREATE}>
+						{t('New content')}
+					</Link>
+				</li>
+				<li className='dropdownList'>
+					<Link to={ROUTES.ADMIN}>
+						{t('Admin')}
+					</Link>
+				</li>
+				<li>
+					<Link to={ROUTES.ABOUT}>
+						{t('About')}
+					</Link>
+				</li>
+				<li className='logoutLi'>
+					<button
+						className='logoutButton'
+						onClick={logout}>
+						{t('Logout')}
+					</button>
+				</li>
+			</ul>
+			<div className='language-button'>
+				<img
+					className='cursor-pointer'
+					src={
+						IMAGE_HOST_PREFIX + 'language/uk.png'
+					}
+					alt='english'
+					onClick={() => changeLanguage('en')}
+				/>
+				<img
+					className='cursor-pointer'
+					src={
+						IMAGE_HOST_PREFIX + 'language/cro.png'
+					}
+					alt='croatian'
+					onClick={() => changeLanguage('cro')}
+				/>
+			</div>
+		</nav>
+	);
 }
 
 export default withRouter(Navbar);
